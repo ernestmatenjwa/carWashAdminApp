0b6a5ff2cece7454c8e23d5557069668542270bf
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2022-02-16 14:12:07 +0200

    staff

--- components/staff.js
+++ components/staff.js
@@ -1,10 +1,23 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import Recat,{useState} from 'react';
+import { Text, View,ScrollView } from 'react-native';
 
 export default function StaffScreen({ navigation }) {
+  const [people,setPeople] =useState([
+    {name:'karabo',jobTitle:'CEO',email:'k@gmail.com',key:'1'},
+    {name:'Alex',key:'2'},
+    {name:'Gomolemo',key:'3'},
+    {name:'Shaun',key:'4'},
+    {name:'Mpho',key:'5'},
+    {name:'Innocent',key:'6'},
+  ]);
   return (
-    <View>
-       <Text>This is StaffScreen screen</Text>
-    </View>
+    <ScrollView>
+      {people.map(item =>(
+        <View key={item.key}>
+          <Text >{item.name}</Text>
+        </View>
+      ))}
+    
+    </ScrollView>
   );
 }
