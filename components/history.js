18d093e27b0e105e05f2cb43906f37cd6894a374
unknown <gomolemosibiya@gmail.com>
2022-02-21 12:49:35 +0200

    history data added

--- components/history.js
+++ components/history.js
@@ -1,66 +1,70 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, { useState } from 'react';
 import { 
     View, 
     StyleSheet, 
     ImageBackground,
     Button,
     Alert,
-    TouchableOpacity
+    TouchableOpacity,
+    FlatList,
+    ScrollView,
+    SafeAreaView,
+    Text
 } from 'react-native';
 import styled from 'styled-components/native';
 import {LinearGradient} from 'expo-linear-gradient'
 import { dummyData, FONTS, SIZES, COLORS, icons, images } from '../constants';
 import { McText, McIcon } from '../component';
-import { 
-  ScrollView,
-  SafeAreaView
-} from 'react-native';
 
-export default function HistoryScreen({ navigation }) {
-  return (
-    <SafeAreaView>
-        <View style={styles.container}>
-          <SectionHeader>
-            <ImageBackground
-              resizeMode='cover'
-              source={images.hero_bg}
-              style = {{
-                width: '100%',
-                //height: SIZES.height < 700 ? SIZES.height * 0.4:SIZES.height * 0.5,
-                height: '100%',
-              }}
-              imageStyle= {{opacity:0.5}}
-            >
-              {/* <McIcon source={icons.back_arrow} size={24} color='#fff' top={25} marginLeft={20}/>
-              <McText h1 style={{left: '35%' }}>History</McText> */}
-            </ImageBackground> 
-          </SectionHeader>
-          <DetailBox>
-            <McText style = {{ fontSize: 20, fontWeight: 'bold', color: '#808080', marginLeft: 5 }}>
-              June 2020
-            </McText>
-            <HistoricalData/>
-          </DetailBox>
-        </View>
-    </SafeAreaView>
-  );
-}
-
-const HistoricalData = () => {
-  return (
-    <>
+const History = () => {
+  
+  const _renderItem = ({item, index}) => {
+    return (
       <View marginVertical={5} marginHorizontal={15}>
-    
+        <TimeDate>
+          <McText body3 style={{color: COLORS.gray, fontSize: 9}}>{item.date}{' '}{item.time}</McText>
+        </TimeDate>
+        <FirstRow style={{flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+          <McText body3 style={{color: COLORS.black}}>{item.vehicleModel}{' - '}{item.vehicleRegistration}</McText>
+          <McText body3 style={{color: COLORS.black}}>{item.vehicleType}{' - '}{item.serviceType}</McText>
+        </FirstRow>
+        <SecondRow style={{flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+          <McText body3 style={{color: COLORS.black}}>{item.customerName}</McText>
+          <McText h3 style={{color: '#0B6F83'}}>R {item.price}</McText>
+        </SecondRow>
       </View>
+    )
+  }
 
-      <View marginVertical={5} marginHorizontal={15}>
-        
-      </View>
-    </>
-
-    
+  return (
+    <View style={styles.container}>
+      <SectionHeader>
+        <ImageBackground
+          resizeMode='cover'
+          source={images.hero_bg}
+          style = {{
+            width: '100%',
+            height: '100%',
+          }}
+          imageStyle= {{opacity:0.5}}
+        >
+          <McIcon source={icons.back_arrow} size={24} color='#fff' top={25} marginLeft={20}/>
+          <McText h1 style={{left: '35%' }}>History</McText>
+        </ImageBackground>
+      </SectionHeader> 
+      <DetailBox >
+        <McText style = {{ fontSize: 20, color: '#808080', marginLeft: 5 }}>
+          June 2020
+        </McText>
+          <FlatList 
+            keyExtractor={(item) => 'event_' + item.id}
+            data={dummyData.HistoricalData}
+            renderItem={_renderItem}
+          />
+      </DetailBox> 
+    </View>
   );
-}
+};
 
 const TimeDate = styled.View`
 `;
@@ -86,8 +90,8 @@ const DetailBox = styled.View`
   border-radius: ${SIZES.radius};
   background-color: ${COLORS.white};
   elevation: 12;
-  top: -80px;
-  left: 20px;
+  top: -80;
+  left: 20;
   padding: 10px;
 `;
 
@@ -100,4 +104,14 @@ const styles = StyleSheet.create({
     backgroundColor:'transparent',
     opacity: 0.6
   },
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
 });
+
+export default History;
